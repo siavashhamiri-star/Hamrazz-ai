027b90a1d8242e145503fb6ccc62c66475a932cc
SiavasHHamiri <siavashhamiri@gmail.com>
2025-11-27 02:09:39 +0000

    همچنین میخواهم چون آپ با کودکان سر و کار دارد یک محیط زصریشاد و یک رابطه

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
-        headline: ['"PT Sans"', 'sans-serif'],
+        body: ['Arial', 'sans-serif'],
+        headline: ['Arial', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
